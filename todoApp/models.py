ddf71d9cdc7fcd7c54f089290bc3906906cbb101
SaberHardy <samirsamado5@gmail.com>
2024-08-11 12:13:14 -0700

    create a todo model

--- todoApp/models.py
+++ todoApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+priorities = (
+    ('H', 'High'),
+    ('M', 'Medium'),
+    ('L', 'Low'),
+    ('N', 'None')
+)
+
+
+class TodoModel(models.Model):
+    title = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(max_length=1000)
+    completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=1, choices=priorities)
+
+    def __str__(self):
+        return self.title
