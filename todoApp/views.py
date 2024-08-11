94223f34bccd5225345cb0dda108115a96b799f7
SaberHardy <samirsamado5@gmail.com>
2024-08-11 12:13:31 -0700

    create a view and url

--- todoApp/views.py
+++ todoApp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from .models import TodoModel
+
 
 def index(request):
-    return render(request, 'todoApp/home.html')
+    todos = TodoModel.objects.all()
+    context = {
+        'todos': todos
+    }
+    return render(request, 'todoApp/home.html', context)
+
+
+def todos(request):
+    return render(request, 'todoApp/todos.html')
